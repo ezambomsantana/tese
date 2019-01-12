84bb7eff9b35c7cd48d9db8bd16105bc2346daec
Eduardo Santana <ezambomsantana@gmail.com>
2019-01-12 16:27:33 -0200

    Update travel_count.R

--- travel_count.R
+++ travel_count.R
@@ -1,16 +1,16 @@
 require("ggplot2")
 
 viagens <- c()
-viagens$tipo <- c("Car",4029,"Bus",5058)
+viagens$tipo <- c("Car",5058,"Bus",4029)
 viagens$count <- c("Subway",3030,"Walk",6287)
 
 df <- data.frame(matrix(unlist(viagens), nrow=4, byrow=T),stringsAsFactors=FALSE)
 
 df['X2'] <- sapply(df['X2'], function(x) as.numeric(x))
 
-png('travel_count.png')
+png('c:/dev/travel_count.png')
 theme_set(theme_gray(base_size = 18))
 ggplot(data=df, aes(x=X1, y=X2, group=1)) +
   geom_bar(stat="identity", fill="#56B4E9") +
-  xlab("Travel Mode") + ylab("Travels Count (x1000)")
+  xlab("Travel Mode") + ylab("Trips Count (x1000)")
 dev.off()
